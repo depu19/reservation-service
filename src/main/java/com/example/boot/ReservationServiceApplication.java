b43f59907c4c3e1bf65cc63dd44174402c2ed71b
S <deepa.s2@hpe.com>
2017-02-12 16:33:39 +0530

    YOLO

--- src/main/java/com/example/boot/ReservationServiceApplication.java
+++ src/main/java/com/example/boot/ReservationServiceApplication.java
@@ -1,6 +1,7 @@
 package com.example.boot;
 
 import java.util.Collection;
+
 import java.util.stream.Stream;
 
 import javax.persistence.Entity;
@@ -8,17 +9,24 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
 	
@@ -46,6 +54,27 @@ public class ReservationServiceApplication {
 		}
 		
 	}
+	
+}
+
+
+
+@RestController
+@RefreshScope
+ class MessageRestController{
+	
+	private final String value;
+
+	@Autowired
+	public MessageRestController(@Value("${message}")String value) {
+		this.value = value;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,value="/message")
+	public String getMessage(){
+		return this.value;
+	}
+	
 	
 }
 
